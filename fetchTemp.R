bc96d0ea212c51d682bf20079913e022b3be2eba
Quang Ha <qth20@bu.edu>
2019-03-12 23:43:58 -0400

    Working on some aspect of fetching temperature

---
+++ fetchTemp.R
@@ -0,0 +1,10 @@
+devtools::install_github("ropensci/rnoaa")
+library("rnoaa")
+
+# Using the input Token
+options(noaakey = "YOUR-TOKEN")
+
+# Search for data and get a data.frame
+out <- ncdc(datasetid='NORMAL_DLY', stationid='GHCND:USW00014895', datatypeid='dly-tmax-normal', startdate = '2010-01-01', enddate = '2010-12-10', limit = 300)
+# Print out the data.frame
+print(out$data)
