e66728e7d93089e0d35f7530c6227deba824c15e
Quang Ha <qth20@bu.edu>
2019-03-14 00:29:25 -0400

    Add argument for datasetiD

--- fetchTemp.R
+++ fetchTemp.R
@@ -5,10 +5,10 @@ library("rnoaa")
 source('loadNOAAToken.R')
 
 # Search for a station using a bounding box
-stations <- ncdc_stations(extent=c(22.0, -83.0, 26.0, -79.0))
+stations <- ncdc_stations(datasetid='NORMAL_DLY', extent=c(22.0, -83.0, 26.0, -79.0))
 
 # Search for data and get a data.frame
-# out <- ncdc(datasetid='NORMAL_DLY', stationid='WPLF1', datatypeid='dly-tmax-normal', startdate = '2010-01-01', enddate = '2010-12-10', limit = 300)
+# out <- ncdc(datasetid='NORMAL_DLY', stationid='GHCND:USC00080414', datatypeid='dly-tmax-normal', startdate = '2010-01-01', enddate = '2010-12-10', limit = 300)
 # Print out the data.frame
 # print(out$data)
 
