1f2530d55fa5cac6cd7cfccba17187a4707130a2
Quang Ha <qth20@bu.edu>
2019-03-13 23:33:25 -0400

    Able to search stations with locations now - need to process this data

--- fetchTemp.R
+++ fetchTemp.R
@@ -4,8 +4,11 @@ library("rnoaa")
 # Load Token
 source('loadNOAAToken.R')
 
+# Search for a station using a bounding box
+stations <- ncdc_stations(extent=c(22.0, -83.0, 26.0, -79.0))
+
 # Search for data and get a data.frame
-out <- ncdc(datasetid='NORMAL_DLY', stationid='GHCND:USW00014895', datatypeid='dly-tmax-normal', startdate = '2010-01-01', enddate = '2010-12-10', limit = 300)
+# out <- ncdc(datasetid='NORMAL_DLY', stationid='WPLF1', datatypeid='dly-tmax-normal', startdate = '2010-01-01', enddate = '2010-12-10', limit = 300)
 # Print out the data.frame
-print(out$data)
+# print(out$data)
 
