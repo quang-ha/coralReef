68579085fa71870401e8ad6ebae4848b2e0e3a9f
Quang Ha <qth20@bu.edu>
2019-03-13 22:55:57 -0400

    Edit to use a Token file

--- fetchTemp.R
+++ fetchTemp.R
@@ -1,10 +1,11 @@
 devtools::install_github("ropensci/rnoaa")
 library("rnoaa")
 
-# Using the input Token
-options(noaakey = "YOUR-TOKEN")
+# Load Token
+source('loadNOAAToken.R')
 
 # Search for data and get a data.frame
 out <- ncdc(datasetid='NORMAL_DLY', stationid='GHCND:USW00014895', datatypeid='dly-tmax-normal', startdate = '2010-01-01', enddate = '2010-12-10', limit = 300)
 # Print out the data.frame
 print(out$data)
+
