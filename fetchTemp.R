98d4a0e359d3e6a6831ebb7e0c7f9d3a096c66e2
Quang Ha <qth20@bu.edu>
2019-03-16 01:23:27 -0400

    Change datasets for higher resolution

--- fetchTemp.R
+++ fetchTemp.R
@@ -5,10 +5,18 @@ library("rnoaa")
 source('loadNOAAToken.R')
 
 # Search for a station using a bounding box
-stations <- ncdc_stations(datasetid='NORMAL_DLY', extent=c(22.0, -83.0, 26.0, -79.0))
+stations <- ncdc_stations(datasetid='NORMAL_DLY', extent=c(24.5, -81.4, 25.2, -80.3))
 
-# Search for data and get a data.frame
-# out <- ncdc(datasetid='NORMAL_DLY', stationid='GHCND:USC00080414', datatypeid='dly-tmax-normal', startdate = '2010-01-01', enddate = '2010-12-10', limit = 300)
-# Print out the data.frame
-# print(out$data)
+# Obtain the stations' IDs
+stations_ids <- stations$data$id
+
+# Loop through each stations and obtain the reading
+for (sid in stations_ids) {
+  print(sid)
+  out <- ncdc(datasetid='GHCND', stationid=sid, datatypeid='TMAX', startdate = '1983-01-01', enddate = '1983-01-31', limit = 300)
+  if (!is.null(out$meta$totalCount)) {
+    print(out$data)    
+  }
+}
+
 
